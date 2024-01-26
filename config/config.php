526e053fe6c14f4bb2bab8f32f499f98549364c3
yazid sefsaf <yazidsefsaf45@yahoo.com>
2024-01-26 14:18:22 +0100

    ok   ok

--- config/config.php
+++ config/config.php
@@ -7,7 +7,7 @@ define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : APP_DB_HOST);
 define('DB_NAME', getenv('DB_NAME') ? getenv('DB_NAME') : APP_DB_NAME);
 
 //View
-define('APP_VIEW_PATH', __DIR__ . '/../src/View/');
+define('APP_VIEW_PATH', __DIR__ . '/../src/View');
 
 // database dump file path for automatic import
 define('DB_DUMP_PATH', __DIR__ . '/../database.sql');
