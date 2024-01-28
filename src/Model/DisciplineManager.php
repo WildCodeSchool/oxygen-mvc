007f32f5f77e430512c14f67b9e412de65cd758b
TRINH <laser2u02@gmail.com>
2024-01-29 00:50:29 +0100

    loading databases

---
+++ src/Model/DisciplineManager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model;
+
+class DisciplineManager extends AbstractManager
+{
+    public const TABLE = 'discipline';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
