8d54a48e3d087fe4d6b3593810f46a6ee4731a18
TRINH <laser2u02@gmail.com>
2024-01-29 22:27:53 +0100

    discipline done

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Model\DisciplineManager;
+
 class HomeController extends AbstractController
 {
     /**
@@ -10,8 +12,16 @@ class HomeController extends AbstractController
     public function index(): string
     {
         $name = 'Oxygen';
+
+        // Initialize the discipline manager
+        $disciplineManager = new DisciplineManager();
+
+        // Get all disciplines
+        $disciplines = $disciplineManager->selectAll();
+
         return $this->twig->render('Home/index.html.twig', [
             'name' => $name,
+            'disciplines' => $disciplines,
         ]);
     }
 }
