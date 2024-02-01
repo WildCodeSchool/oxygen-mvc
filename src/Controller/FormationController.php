1c01a0a15c09bf3cd3c2f95deffb6d4b3e3515ba
yazid sefsaf <yazidsefsaf45@yahoo.com>
2024-02-01 13:48:27 +0100

    recuperation des cours de la base de donnes dans la page descipline et la page formation et regmage de background de inputs

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -2,11 +2,18 @@
 
 namespace App\Controller;
 
+use App\Model\FormationManager;
+
 class FormationController extends AbstractController
 {
-    public function index(): string
+    public function show($id): string
     {
+        //get the course whzere id course = id course in the url
+        $CoursesManager = new FormationManager();
+        $formations = $CoursesManager->selectOneById($id);
 
-        return $this->twig->render('formation/index.html.twig');
+        return $this->twig->render('formation/show.html.twig', [
+            'formation' => $formations
+        ]);
     }
 }
