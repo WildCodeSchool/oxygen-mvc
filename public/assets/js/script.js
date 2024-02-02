57bf261fb88a76971c05598e155a6ebf88ebca06
TRINH <laser2u02@gmail.com>
2024-02-02 01:16:31 +0100

    fix back top btn

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,30 +1,4 @@
 document.addEventListener( 'DOMContentLoaded', function() {
-
-    // BACK TO TOP
-    function handleBackToTop() {
-        const backToTopButton = document.querySelector('[data-backtop]');
-    
-        function handleBackToTopVisibility() {
-          if (window.scrollY >= 200) {
-            backToTopButton.style.display = 'block';
-          } else {
-            backToTopButton.style.display = 'none';
-          }
-        }
-        function scrollToTop() {
-          window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-          });
-        }
-    
-        window.addEventListener('scroll', handleBackToTopVisibility);
-        backToTopButton.addEventListener('click', scrollToTop);
-        handleBackToTopVisibility();
-    }
-    handleBackToTop();
-
-
     // SETUP OPTION SLIDE SHOW
     const splide = new Splide( '.splide', {
         type   : 'loop',
@@ -44,4 +18,4 @@ document.addEventListener( 'DOMContentLoaded', function() {
         arrows: false,     
     });
     splide.mount();
-} );
+});
