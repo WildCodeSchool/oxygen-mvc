422d5b6698e61878fa5a26e95190bde0da8befc9
TRINH <laser2u02@gmail.com>
2024-02-03 16:04:16 +0100

    fix error

--- src/Controller/DisciplineController.php
+++ src/Controller/DisciplineController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Model\FormationManager;
+use App\Model\DisciplineManager;
 
 class DisciplineController extends AbstractController
 {
@@ -11,12 +12,18 @@ class DisciplineController extends AbstractController
      */
     public function index(): string
     {
+        // Initialize the discipline manager
+        $disciplineManager = new DisciplineManager();
+
+        // Get all disciplines
+        $disciplines = $disciplineManager->selectAll();
 
         $coursesMananager = new FormationManager();
         $courses = $coursesMananager->selectAll();
 
         return $this->twig->render('discipline/index.html.twig', [
             'courses' => $courses,
+            'disciplines' => $disciplines,
         ]);
     }
 }
