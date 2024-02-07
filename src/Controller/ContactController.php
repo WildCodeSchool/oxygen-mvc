57e05f8e77abbb041f1377c62883a37077fe37a5
assma8jaballah <assma.jaballah@gmail.com>
2024-02-07 15:22:54 +0100

    securisation done for contact form

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -11,22 +11,98 @@ class ContactController extends AbstractController
      */
     public function index(): ?string
     {
-
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // clean $_POST data
-            $contact = array_map('trim', $_POST);
-
-            // TODO validations (length, format...)
-
-            // if validation is ok, insert and redirection
-            $contactManager = new ContactManager();
-            $contactManager->insert($contact);
-
-            header('Location:/contact');
-
-            return null;
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            return $this->twig->render('Contact/contact.html.twig');
         }
 
-            return $this->twig->render('Contact/contact.html.twig');
+        foreach ($_POST as $key => $value) {
+            $_POST[$key] = trim(htmlentities($value));
+        }
+
+            $errors = $this->validationForm();
+
+        if (empty($errors)) {
+            (new ContactManager())->insert($_POST);
+            header('Location: /contact/message');
+            exit();
+        }
+
+            return $this->twig->render('Contact/contact.html.twig', [
+                'errors' => $errors,
+                'post' => $_POST,
+            ]);
+    }
+// la méthode message() affiche un message de confirmation en cas de succes.
+    public function message(): ?string
+    {
+        return $this->twig->render(
+            'Contact/contact.html.twig',
+            ['success' => 'Merci ! nous avons bien reçu votre message, nous vous répondrons dans les plus brefs délais.'
+            ]
+        );
+    }
+// la méthode validationForm() permet de valider les champs du formulaire.
+    private function validationForm(): array
+    {
+        $errors = [];
+
+        $this->validateRequired(['firstName', 'lastName', 'message'], $errors);
+        $this->validateFieldLength('firstName', 3, 200, 'Veuillez ajouter un prénom valide 
+        d\'au moins 3 caractères !', $errors);
+        $this->validateFieldLength('lastName', 3, 200, 'Veuillez ajouter un nom valide 
+        d\'au moins 3 caractères !', $errors);
+        $this->validateMail('email', 'Veuillez ajouter une adresse mail valide !', $errors);
+        $this->validatePhoneNumber('phone', 'Veuillez ajouter un numéro de téléphone valide !', $errors);
+        $this->validateFieldLength('message', 10, 4000, 'Votre message doit contenir au moins 10 caractères', $errors);
+
+        return $errors;
+    }
+// la méthode validateRequired() permet de valider les champs obligatoires.
+    private function validateRequired(array $fields, array &$errors): void
+    {
+        foreach ($fields as $field) {
+            if (empty($_POST[$field])) {
+                match ($field) {
+                    'firstName' => $field = 'prénom',
+                    'lastName' => $field = 'nom',
+                    'message' => $field = 'message',
+                    default => $field = 'champ',
+                };
+
+                    $errors[] = "Votre {$field} est obligatoire";
+            }
+        }
+    }
+// la méthode validateFieldLength() permet de valider la longueur des champs.
+
+    private function validateFieldLength(
+        string $field,
+        int $minLength,
+        int $maxLength,
+        string $errorMessage,
+        array &$errors
+    ): void {
+        $value = $_POST[$field] ?? '';
+        $length = mb_strlen($value, 'UTF-8');
+
+        if (!empty($value) && ($length < $minLength || $length > $maxLength)) {
+            $errors[] = $errorMessage;
+        }
+    }
+// la méthode validateMail() permet de valider l'adresse mail.
+    private function validateMail(string $field, string $errorMessage, array &$errors): void
+    {
+        if (!filter_var($_POST[$field], FILTER_VALIDATE_EMAIL)) {
+            $errors[] = $errorMessage;
+        }
+    }
+// la méthode validatePhoneNumber() permet de valider le numéro de téléphone.
+
+    private function validatePhoneNumber(string $field, string $errorMessage, array &$errors): void
+    {
+
+        if (!preg_match('/^[0-9]{10,10}$/', $_POST[$field])) {
+            $errors[] = $errorMessage;
+        }
     }
 }
