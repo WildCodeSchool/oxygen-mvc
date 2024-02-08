5084c4c000e87b2c518264abcb198862438a6c3e
TRINH <laser2u02@gmail.com>
2024-02-08 03:27:52 +0100

    add course demo

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -59,15 +59,11 @@ class AdminController extends AbstractController
     }
     public function formation(): string
     {
-
         // Initialize the formation manager
         $formationManager = new FormationManager();
 
         // Get all formations
         $courses = $formationManager->selectAll();
-
-        // Total number of formations
-        $totalCourses = count($courses);
 
         // Initialize the discipline manager
         $disciplineManager = new DisciplineManager();
@@ -87,6 +83,30 @@ class AdminController extends AbstractController
 
         // Total number of formations
         $totalCourses = count($filteredCourses);
+
+        // Check if the form is submitted
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Clean $_POST data
+            $courseData = [
+                'name' => $_POST['name-course'],
+                'description' => $_POST['courseDes'],
+                'capacity' => $_POST['capacity'],
+                'location' => $_POST['location'],
+                'date' => $_POST['date-course'],
+                'duration' => $_POST['duration'],
+                'degree' => $_POST['degree'],
+                'financing_supported' => isset($_POST['support']) ? true : false,
+                'discipline_id' => isset($_POST['discipline']) ? intval($_POST['discipline']) : null,
+                'url_image' => $_POST['image-url'],
+            ];
+
+            // Insert course data into the database
+            $formationManager->insert($courseData);
+
+            // Redirect to prevent form resubmission
+            header('Location: /admin/formation');
+            exit;
+        }
 
         return $this->twig->render('Admin/formation/formation.html.twig', [
             'title' => 'Formation',
--- src/Model/FormationManager.php
+++ src/Model/FormationManager.php
@@ -23,4 +23,45 @@ class FormationManager extends AbstractManager
 
         return $statement = $statement->fetchAll();
     }
+
+    // Insert new course
+    public function insert(array $course): int
+    {
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (name, description, capacity, location, date, duration, degree, financing_supported, discipline_id, url_image) 
+            VALUES (:name, :description, :capacity, :location, :date, :duration, :degree, :financing_supported, :discipline_id, :url_image)");
+
+        $statement->bindValue('name', $course['name'], \PDO::PARAM_STR);
+        $statement->bindValue('description', $course['description'] ?? null, \PDO::PARAM_STR);
+        $statement->bindValue('capacity', $course['capacity'] ?? null, \PDO::PARAM_STR);
+        $statement->bindValue('location', $course['location'] ?? null, \PDO::PARAM_STR);
+        $statement->bindValue('date', $course['date'] ?? null, \PDO::PARAM_STR);
+        $statement->bindValue('duration', $course['duration'] ?? null, \PDO::PARAM_STR);
+        $statement->bindValue('degree', $course['degree'] ?? null, \PDO::PARAM_STR);
+        $statement->bindValue('financing_supported', isset($course['support']) ? true : false, \PDO::PARAM_BOOL);
+        $statement->bindValue('discipline_id', $course['discipline_id'] ?? null, \PDO::PARAM_INT);
+        $statement->bindValue('url_image', $course['url_image'] ?? null, \PDO::PARAM_STR);
+
+        $statement->execute();
+        return (int)$this->pdo->lastInsertId();
+    }
+
+    // Update course
+    public function update(array $course): void
+    {
+        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET name = :name, description = :description, capacity = :capacity, location = :location, date = :date, duration = :duration, degree = :degree, financing_supported = :financing_supported, discipline_id = :discipline_id, url_image = :url_image WHERE id=:id");
+
+        $statement->bindValue('id', $course['id'], \PDO::PARAM_INT);
+        $statement->bindValue('name', $course['name'], \PDO::PARAM_STR);
+        $statement->bindValue('description', $course['description'] ?? null, \PDO::PARAM_STR);
+        $statement->bindValue('capacity', $course['capacity'] ?? null, \PDO::PARAM_STR);
+        $statement->bindValue('location', $course['location'] ?? null, \PDO::PARAM_STR);
+        $statement->bindValue('date', $course['date'] ?? null, \PDO::PARAM_STR);
+        $statement->bindValue('duration', $course['duration'] ?? null, \PDO::PARAM_STR);
+        $statement->bindValue('degree', $course['degree'] ?? null, \PDO::PARAM_STR);
+        $statement->bindValue('financing_supported', isset($course['support']) ? true : false, \PDO::PARAM_BOOL);
+        $statement->bindValue('discipline_id', $course['discipline_id'] ?? null, \PDO::PARAM_INT);
+        $statement->bindValue('url_image', $course['url_image'] ?? null, \PDO::PARAM_STR);
+
+        $statement->execute();
+    }
 }
